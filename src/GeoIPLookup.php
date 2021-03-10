9ba9523926ed459943a04d238823a0ca3ca56a68
doggie <doggieliu@xforce-games.com>
2021-03-10 17:14:49 +0800

    Added PHP 5.6 support

--- src/GeoIPLookup.php
+++ src/GeoIPLookup.php
@@ -1,32 +1,63 @@
 <?php
 
-namespace Ayesh\GeoIP;
-
-class GeoIPLookup {
-    private string $data_dir;
-    public function __construct(string $data_dir) {
-        $this->data_dir = rtrim($data_dir, '/\\');
+namespace {
+    if (!class_exists('JsonException')) {
+        class JsonException extends Exception
+        {
+        }
     }
 
-    public static function createFromDefaultDatabase(): self {
-        return new static(__DIR__ . '/../../geo-ip-database/data');
+    if (!defined('JSON_THROW_ON_ERROR')) {
+        define('JSON_THROW_ON_ERROR', 4194304);
     }
+}
 
-    public function lookup(string $ip_address): ?string {
-        $parts = explode('.', $ip_address, 2);
-        $file = $this->data_dir . '/' . $parts[0] . '.json';
-        $map = file_get_contents($file);
-        $map = json_decode($map, true, 3, JSON_THROW_ON_ERROR);
-        $find = ip2long('0.' . $parts[1]);
-        $last = null;
-        foreach ($map as $long_val => $code) {
-            if ($find >= $long_val) {
-                $last = $code;
-                continue;
-            }
-            return $last;
+namespace Ayesh\GeoIP {
+
+    class GeoIPLookup {
+        /**
+         * @var string
+         */
+        private $data_dir;
+
+        private $maps = [];
+
+        public function __construct($data_dir) {
+            $this->data_dir = rtrim($data_dir, '/\\');
         }
 
-        return $last;
+        public static function createFromDefaultDatabase() {
+            return new static(__DIR__ . '/../../../ayesh/geo-ip-database/data');
+        }
+
+        public function lookup($ip_address) {
+            $parts = explode('.', $ip_address, 2);
+
+            if (isset($this->maps[$parts[0]])) {
+                $map = $this->maps[$parts[0]];
+            }
+            else {
+                $file = $this->data_dir . '/' . $parts[0] . '.json';
+                $data = file_get_contents($file);
+                $map = json_decode($data, true, 3, JSON_THROW_ON_ERROR);
+                if ($map === false) {
+                    throw new \JsonException(json_last_error());
+                }
+                $this->maps[$parts[0]] = $map;
+            }
+
+            $find = ip2long('0.' . $parts[1]);
+            $last = null;
+            foreach ($map as $long_val => $code) {
+                if ($find >= $long_val) {
+                    $last = $code;
+                    continue;
+                }
+                return $last;
+            }
+
+            return $last;
+        }
     }
+
 }
